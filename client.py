f059bd848eff9a4eb80b0c3bc3b37241b776e6f3
marucg <marucarrillo08@gmail.com>
2019-05-04 22:54:57 +0200

    Client page

--- client.py
+++ client.py
@@ -6,16 +6,29 @@ import http.client
 PORT = 8000
 SERVER = '127.0.0.1'
 
-# PARAMETERS ALREADY ESTABLISHED
-endpoints = ['/listSpecies?limit=5&json=1', '/listSpecies?json=1', '/karyotype?specie=mouse&json=1',
-             '/chromosomeLength?specie=mouse&chromo=12&json=1', '/geneSeq?gene=frat1&json=1',
-             '/geneInfo?gene=frat1&json=1', '/geneCalc?gene=frat1&json=1',
-             '/geneList?chromo=1&start=0&end=30000&json=1']
+# INTRODUCING THE PARAMETERS
+
+limit = input('Introduce the limit parameter for obtaining species: ')
+specie = input('Introduce a specie for obtaining the karyotype: ')
+specie_chromo = input('Introduce a specie for obtaining the length of its chromosome: ')
+chromo = input('Introduce the chromosome for obtaining length: ')
+seq = input('Introduce a gene for obtaining its sequence: ')
+info = input('Introduce a gene for obtaining information: ')
+calc = input('Introduce a gene for some calculations: ')
+gene_chromo = input('Introduce a chromosome for obtaining its genes names: ')
+start = input('Introduce the start position: ')
+end = input('Introduce the end position: ')
+
+endpoints = ['/listSpecies?limit=' + limit + '&json=1', '/listSpecies?json=1', '/karyotype?specie=' + specie + '&json=1',
+            '/chromosomeLength?specie=' + specie_chromo + '&chromo=' + chromo + '&json=1', '/geneSeq?gene=' + seq + '&json=1',
+            '/geneInfo?gene=' + info + '&json=1', '/geneCalc?gene=' + calc + '&json=1',
+            '/geneList?chromo=' + gene_chromo + '&start=' + start + '&end=' + end + '&json=1']
+
 
 
 print("\nConnecting to server: \n", 'Server:', SERVER + ', Port:', PORT)
 conn = http.client.HTTPConnection(SERVER, PORT)
-print('---Information obtained---')
+print('\n---Information obtained---\n')
 
 # LOOP FOR OBTAINING EVERY ENDPOINT INCLUDED IN THE LIST
 for data in endpoints:
